ec94c596651304b830a707b372404abde2fc2da1
ManuLEM <manuel.lemaire@gmail.com>
2017-07-06 16:53:34 +0200

    Improve overall style and area look

--- app/directives/rateEvolution.js
+++ app/directives/rateEvolution.js
@@ -5,30 +5,41 @@ app.directive('rateEvolution', function(){
     template: '<div id="chart"></div>',
     replace: true,
     scope: {
-      transactions: '='
+      ngModel: '='
     },
     link: function(scope, elem){
-      scope.transactions = scope.transactions.filter(function(elem){
-        console.log(moment(elem.details.completed), moment().startOf('day').add(11, 'hours'));
-        return moment(elem.details.completed).isAfter(moment().startOf('day').add(11, 'hours'))
-      });
-      console.log(scope.transactions);
+      setInterval(function(){
+        console.log("test");
+        setTimeout(function(){
+          chart.series[0].addPoint({
+            y: Math.round(Math.random()*300)/100,
+            x: moment()
+          });
+        })
+      }, 6000)
+      scope.transactions = scope.ngModel;
       var ceil = 0;
+      var transactionsByMin = {};
+      var key = null;
+      var data = [];
       scope.transactions.forEach(function(elem){
-        ceil = Math.max(ceil, parseInt(elem.details.new_balance.amount) + 100)
-      });
-      var data = scope.transactions.map(function(elem){
+        ceil = Math.max(ceil, parseInt(elem.details.new_balance.amount) + 100);
+      data = scope.transactions.map(function(elem){
         return {
           y: Math.round((ceil - parseInt(elem.details.new_balance.amount))/ceil*300, 2) / 100,
           x: moment(elem.details.completed).unix()
         }
-      })
-      .sort(function(a,b){
+      });
+      console.log(data);
+      data.sort(function(a,b){
         if (a.x > b.x) return 1;
         if (a.x < b.x) return -1;
         return 0;
       });
       var options = {
+        credits: {
+          enabled: false
+        },
         title: {
           text: null
         },
@@ -36,12 +47,49 @@ app.directive('rateEvolution', function(){
           enabled: false
         },
         xAxis: {
-          visible: false
+          visible: false,
+          scrollbar: {
+            enabled: true
+          },
+          min: data[data.length-1].x - 200,
+          max: data[data.length-1].x
         },
         yAxis: {
-          visible: false
+          visible: false,
+          min: 0
         },
+        chart: {
+          spacingLeft: 0,
+          spacingRight: 0,
+          margin: [0, 50, 0, 0],
+          backgroundColor: 'transparent'
+        },
+        plotOptions: {
+          series: {
+            states: {
+              hover: {
+                enabled: false
+              }
+            }
+          }
+        },
+        tooltip: { enabled: false },
         series: [{
+          scrollbar: {
+            enabled: true,
+            barBackgroundColor: 'gray',
+            barBorderRadius: 7,
+            barBorderWidth: 0,
+            buttonBackgroundColor: 'gray',
+            buttonBorderWidth: 0,
+            buttonArrowColor: 'yellow',
+            buttonBorderRadius: 7,
+            rifleColor: 'yellow',
+            trackBackgroundColor: 'white',
+            trackBorderWidth: 1,
+            trackBorderColor: 'silver',
+            trackBorderRadius: 7
+          },
           dataGrouping: {
             smoothed: true,
             forced: true,
@@ -51,13 +99,12 @@ app.directive('rateEvolution', function(){
           marker: {
             enabled:false
           },
-          name: 'Other',
           data: data,
-          lineColor: '#7FCFDC',
+          lineColor: '#89FFA4',
           fillColor: '#3F496C'
         }]
       };
-      Highcharts.chart('chart', options)
+      var chart = Highcharts.chart('chart', options);
     }
   }
 });
